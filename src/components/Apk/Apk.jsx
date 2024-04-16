15e644ebcebeb0871f230d0c479ea2c1a6d96fe5
christian magallanes <desarrollowebfront@gmail.com>
2024-04-16 03:50:34 -0300

    hola

--- src/components/Apk/Apk.jsx
+++ src/components/Apk/Apk.jsx
@@ -10,8 +10,8 @@ const Apk = () => {
           <div className="card">
           <h2 className='card-title'>Minecraft bedrock</h2>
           <img src="https://images.sftcdn.net/images/t_app-icon-m/p/18daa71a-96d1-11e6-a693-00163ed833e7/1605634425/minecraft-download-minecraft.jpg" alt="" />
-          <p className='card-text'>Descripción de la aplicación</p>
-          <a className='btn-card' href="https://www.mediafire.com/" target='_blank'>descargar</a>
+          <p className='card-text'>VERSION OFICIAL 1.20.73 con INICIO de SESIÓN para MINECRAFT BEDROCK ✅</p>
+          <a className='btn-card' href="https://exe.io/B5VL9" target='_blank'>descargar</a>
           </div>
         </div>
     </div>
